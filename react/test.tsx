20fa4998672d162ca1a811dcf6c9181e2a0688a5
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-09-14 16:13:27 +0900

    Feat: 기본 리액트 타입 확인 및 추가 (#4)
    * Feat: 기본 리액트 타입 확인 및 추가
    
    * Fix: 이벤트 핸들러 반환 타입 추가

---
+++ react/test.tsx
@@ -0,0 +1,53 @@
+import {
+  ChangeEventHandler,
+  FormEventHandler,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
+
+const WordRelay = () => {
+  const [word, setWord] = useState('Hello');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  const onSubmitForm: FormEventHandler<HTMLFormElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      const input = inputEl.current;
+      if (word[word.length - 1] === value[0]) {
+        setResult('Correct');
+        setWord(value);
+        setValue('');
+        if (input) {
+          input.focus();
+        }
+      } else {
+        setResult('Wrong');
+        setValue('');
+        if (input) {
+          input.focus();
+        }
+      }
+    },
+    [word, value]
+  );
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    setValue(e.currentTarget.value);
+  }, []);
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputEl} value={value} onChange={onChange} />
+        <button>Submit</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+
+export default WordRelay;
